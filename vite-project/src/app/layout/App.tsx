fafa61ac86d011eff6b0f8a77e51679e3816a41a
Paul Van Leuven <vanleuvenp@pella.com>
2024-09-17 16:54:51 -0500

    end of section 8 fix

--- vite-project/src/app/layout/App.tsx
+++ vite-project/src/app/layout/App.tsx
@@ -1,10 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponents from "./LoadingComponent";
-import { useStore } from "../stores/store";
 import { observer } from "mobx-react-lite";
 import { Outlet, useLocation } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
